69b963b797899e0c430a610fee35208d7665ca6c
GODGEARHD <godgearhd22@gmail.com>
2021-12-20 15:37:27 +0100

    TFG

--- start.php
+++ start.php
@@ -1,5 +1,12 @@
 <?php
-    echo $_POST['nombre'];
-    echo $_POST['email'];
-    echo $_POST['telefono'];
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    if ($nombre == ''){
+        echo 'Hay algún campo vacío';
+    }else {
+        echo $_POST['nombre'];
+        echo $_POST['email'];
+        echo $_POST['telefono'];
+    }
 ?>
